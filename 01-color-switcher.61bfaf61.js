f451f2727494ac4d175bfbe9c696ac53c9e56005
Darina202 <138057834+Darina202@users.noreply.github.com>
2023-11-27 18:30:27 +0000

    Deploying to gh-pages from @ Darina202/goit-js-hw-09@f3075628f7cdfdb4049671f481a8256846ff7fa4 🚀

---
+++ 01-color-switcher.61bfaf61.js
@@ -0,0 +1,2 @@
+let t;const e=document.querySelector("[data-start]"),d=document.querySelector("[data-stop]");function a(){let t=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`;document.body.style.backgroundColor=t}e.addEventListener("click",()=>{t=setInterval(a,1e3),e.disabled=!0,d.disabled=!1}),d.addEventListener("click",()=>{clearInterval(t),e.disabled=!1,d.disabled=!0});
+//# sourceMappingURL=01-color-switcher.61bfaf61.js.map
